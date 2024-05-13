a4bc7d3d1d14d9eae836d29c189d6e50fc32e66e
whos-bax <bbak9604@gmail.com>
2024-05-13 16:51:04 +0900

    home reset

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,19 @@
 import { NextPage } from "next";
+import styles from './page.module.scss';
 import Link from "next/link";
+import {redirect} from "next/navigation";
 
 const NotFound: NextPage = () => {
-    return (
-        <div>
-            <div>
-                이 페이지는 존재하지 않습니다. 다른 페이지를 검색해 보세요.
-            </div>
-            <Link href={`/`}>검색</Link>
-        </div>
-    )
+    redirect('/')
+    return null;
+    // return (
+    //     <div className={styles.container}>
+    //         <div>
+    //             이 페이지는 존재하지 않습니다. 다른 페이지를 검색해 보세요.
+    //         </div>
+    //         <Link href={`/`}>검색</Link>
+    //     </div>
+    // )
 }
 
 export default NotFound
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,17 +18,19 @@ export default function Home() {
     const timerRef = useRef<ReturnType<typeof setInterval> | null>(null);
 
     // 1.5 -> 3, 3 -> 4.5, 4.5 -> 6, 6 -> 7.5
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState<number>(-1);
 
     useLayoutEffect(() => {
         const countOut = sessionStorage.getItem('count-out');
         if (countOut && countOut === 'true') {
             setCount(14)
+        } else {
+            setCount(0);
         }
     }, []);
 
     useEffect(() => {
-        if (count <= 14) {
+        if (count > -1 && count <= 14) {
             timerRef.current = setInterval(() => {
                 setCount((prev) => prev + 0.01);
             }, 10);
@@ -55,7 +57,11 @@ export default function Home() {
         count <= 4 ? mentionList[0]
             : count <= 6.5 ? mentionList[1]
                 : count <= 9 ? mentionList[2]
-                    : mentionList[3]
+                    : mentionList[3];
+
+    const handleResetButton = () => {
+        setCount(0)
+    }
 
     return (
         <main className={styles.container}>
@@ -68,37 +74,51 @@ export default function Home() {
                 priority={true}
                 // onLoad={imageOnLoad}
             />
-            <div className={styles.overlay}>
-                {count < 12 ? (
-                    <p className={cx(styles.overlayText)} style={countValid ? {} : {animation: "none"}}>
-                        {scrollMention}
-                    </p>
-                ) : (
-                    <div className={cx(styles.overlayAllTextDiv)}>
-                        {mentionList.map((mention, idx) => (
-                            <p key={idx} className={styles.overlayAllText}>
-                                {mention}
-                            </p>
-                        ))}
-                    </div>
-                )}
+            {count > -1 &&
+                <div className={styles.overlay}>
+                    {count < 12 ? (
+                        <p className={cx(styles.overlayText)} style={countValid ? {} : {animation: "none"}}>
+                            {scrollMention}
+                        </p>
+                    ) : (
+                        <div className={cx(styles.overlayAllTextDiv)}>
+                            {mentionList.map((mention, idx) => (
+                                <p key={idx} className={styles.overlayAllText}>
+                                    {mention}
+                                </p>
+                            ))}
+                        </div>
+                    )}
 
-                {count < 13 ? (
-                    <Progress
-                        aria-label="Loading..."
-                        className={styles.progress}
-                        style={count > 12 ? {
-                            width: `${(((13 - count) / 2) * 100).toFixed(5)}%`
-                        } : {}}
-                        size="sm"
-                        value={Math.round(count / 12 * 100)}
-                    />
-                ) : (
-                    <Link href={'/about'} className={cx(styles.progress, styles.progressButton)}>
-                        ABOUT ME
-                    </Link>
-                )}
-            </div>
+                    {count < 13 ? (
+                        <Progress
+                            aria-label="Loading..."
+                            className={styles.progress}
+                            style={count > 12 ? {
+                                width: `${(((13 - count) / 2) * 100).toFixed(5)}%`
+                            } : {}}
+                            size="sm"
+                            value={Math.round(count / 12 * 100)}
+                        />
+                    ) : (
+                        <Link href={'/about'} className={cx(styles.progress, styles.progressButton)}>
+                            ABOUT ME
+                        </Link>
+                    )}
+                </div>
+            }
+
+            {count >= 14 && (
+                <button
+                    className={styles.resetButton}
+                    onClick={handleResetButton}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path
+                            d="M125.7 160H176c17.7 0 32 14.3 32 32s-14.3 32-32 32H48c-17.7 0-32-14.3-32-32V64c0-17.7 14.3-32 32-32s32 14.3 32 32v51.2L97.6 97.6c87.5-87.5 229.3-87.5 316.8 0s87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3s-163.8-62.5-226.3 0L125.7 160z"/>
+                    </svg>
+                </button>
+            )}
         </main>
     );
 }
