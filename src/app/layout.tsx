bac5ec81f65f20cbb6fc34bb671d47307a31ed62
whos-bax <bbak9604@gmail.com>
2024-05-13 19:54:09 +0900

    html lang ko

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="ko">
         <body className={inter.className}>
         <Providers>
             <Header/>
