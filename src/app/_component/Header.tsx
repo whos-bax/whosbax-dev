54cdbea1684eaaa794b8f69e4f512b5b310cde63
whos-bax <bbak9604@gmail.com>
2024-05-16 21:29:51 +0900

    header navber mobile

--- src/app/_component/Header.tsx
+++ src/app/_component/Header.tsx
@@ -5,6 +5,8 @@ import cx from "classnames";
 import {utils} from "@/app/_utils/utils";
 import Link from "next/link";
 import {usePathname} from "next/navigation";
+import Icons from "@/app/_component/headerIcon/icons";
+import {useState} from "react";
 
 export default function Header() {
     const pathname = usePathname();
@@ -17,14 +19,17 @@ export default function Header() {
             title: 'guestbook',
             link: '/guestbook'
         }
-    ]
+    ];
+
+    const [barShow, setBarShow] = useState(false);
+    const clickBarShow = () => setBarShow(!barShow);
     return (
         <nav className={styles.header}>
             <div className={styles.container}>
                 <Link href={`/`} className={styles.mainTitle}>
                     {utils.title}
                 </Link>
-                <ul className={styles.headerItemsList}>
+                <ul className={cx(styles.headerItemsList)}>
                     {linkItems.map((item, idx) => (
                         <li className={cx(pathname === item.link && styles.pageBorderBottom)} key={idx}>
                             <Link href={item.link}>
@@ -32,6 +37,26 @@ export default function Header() {
                             </Link>
                         </li>
                     ))}
+                </ul>
+                <ul className={cx(styles.headerItemsList, styles.mobile)}>
+                    {!barShow ? (
+                        <button className={styles.headerIcon} onClick={clickBarShow}>
+                            <Icons.hamburger/>
+                        </button>
+                    ) : (
+                        <button className={styles.headerIcon} onClick={clickBarShow}>
+                            <Icons.close/>
+                        </button>
+                    )}
+                    <div className={cx(styles.headerMobileList, barShow && styles.headerMobileListShow)}>
+                        {linkItems.map((item, idx) => (
+                            <li className={cx(pathname === item.link && styles.pageBorderBottom)} key={idx}>
+                                <Link href={item.link}>
+                                    {item.title.toUpperCase()}
+                                </Link>
+                            </li>
+                        ))}
+                    </div>
                 </ul>
             </div>
         </nav>
