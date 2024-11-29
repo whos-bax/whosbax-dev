2ecfd8dbfab60afd56812a808cdb8e2652d70cfe
whos-bax <bbak9604@gmail.com>
2024-11-29 13:21:46 +0900

    update

--- src/app/about/_component/Education.tsx
+++ src/app/about/_component/Education.tsx
@@ -1,20 +1,19 @@
-import styles from "@/app/about/about.module.scss";
-import {skillList} from "@/app/_utils/utils";
+import styles from '@/app/about/about.module.scss';
+import { skillList } from '@/app/_utils/utils';
 
 type Props = {
-    title: string;
-}
+  title: string;
+};
 
-export default function Education({title}: Props) {
-    return (
-        <div className={styles.columnComponent}>
-            <h4 className={styles.componentTitle}>
-                {title.toUpperCase()}
-            </h4>
+export default function Education({ title }: Props) {
+  return (
+    <div className={styles.columnComponent}>
+      <h4 className={styles.componentTitle}>{title.toUpperCase()}</h4>
 
-            <div className={styles.educationList}>
-                <li>고려대학교(세종) 수학과 : 2015.03 - 2023.02</li>
-            </div>
-        </div>
-    )
+      <div className={styles.educationList}>
+        <li>고려대학교(세종) 수학과 : 2015.03 - 2023.02</li>
+        <li>원티드 프리온보딩 프론트엔드 챌린지 : 2024.05</li>
+      </div>
+    </div>
+  );
 }
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,21 +1,23 @@
 import styles from './about.module.scss';
-import Profile from "@/app/about/_component/Profile";
-import Introduce from "@/app/about/_component/Introduce";
-import cx from "classnames";
-import Experience from "@/app/about/_component/Experience";
-import Skill from "@/app/about/_component/Skill";
-import Education from "@/app/about/_component/Education";
-import Certificate from "@/app/about/_component/Certificate";
+import Profile from '@/app/about/_component/Profile';
+import Introduce from '@/app/about/_component/Introduce';
+import cx from 'classnames';
+import Experience from '@/app/about/_component/Experience';
+import Skill from '@/app/about/_component/Skill';
+import Education from '@/app/about/_component/Education';
+import Certificate from '@/app/about/_component/Certificate';
+import Activities from '@/app/about/_component/Activities';
 
 export default function About() {
-    return (
-        <main className={cx(styles.container, styles.containerGap)}>
-            <Profile/>
-            <Introduce title={'Introduce'}/>
-            <Experience title={'Experience'}/>
-            <Skill title={'Skill'}/>
-            <Education title={'Education'}/>
-            <Certificate title={'Certificate'}/>
-        </main>
-    )
+  return (
+    <main className={cx(styles.container, styles.containerGap)}>
+      <Profile />
+      <Introduce title={'Introduce'} />
+      <Experience title={'Experience'} />
+      <Skill title={'Skill'} />
+      <Education title={'Education'} />
+      <Certificate title={'Certificate'} />
+      <Activities title={'Activities'} />
+    </main>
+  );
 }
